7091235a522c431594e0cb7f1e7cd2fd613cb550
sj <1453648423@qq.com>
2022-01-20 16:22:53 +0800

    新增ResponseBody、url请求参数异常处理

--- src/main/java/com/smart/mall/core/GlobalExceptionAdvice.java
+++ src/main/java/com/smart/mall/core/GlobalExceptionAdvice.java
@@ -7,12 +7,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionAdvice {
@@ -57,4 +61,54 @@ public class GlobalExceptionAdvice {
         UnifyResponse resp = new UnifyResponse(e.getCode(), message, RequestURL);
         return new ResponseEntity<UnifyResponse>(resp, httpStatus);
     }
+
+    /**
+     * 处理ResponseBody请求体参数异常(优先级高于url)
+     * "message":"密码不一致,age:最大不能超过200,name:长度需要在1和10之间",
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public UnifyResponse handleMethodArgumentNotValidException(HttpServletRequest req, MethodArgumentNotValidException e){
+        int code = 9988;
+        StringBuffer stringBuffer = new StringBuffer();
+        e.getBindingResult().getAllErrors().forEach(t -> {
+            if(t instanceof FieldError){
+                String field = ((FieldError) t).getField();
+                stringBuffer.append(field).append(":");
+            }
+            stringBuffer.append(t.getDefaultMessage()).append(",");
+        });
+        String message = stringBuffer.substring(0, stringBuffer.length() - 1);
+        String RequestURL = req.getMethod() + " " + req.getRequestURI();
+        return new UnifyResponse(code, message, RequestURL);
+    }
+
+    /**
+     * 处理url请求参数异常
+     * "message": "id:最大不能超过100,page:最大不能超过10"
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseBody
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public UnifyResponse handleConstraintViolationException(HttpServletRequest req, ConstraintViolationException e){
+        int code = 9988;
+        StringBuffer stringBuffer = new StringBuffer();
+        e.getConstraintViolations().forEach(t -> {
+            String methodAndParam = t.getPropertyPath().toString();
+            String param = methodAndParam.substring(methodAndParam.indexOf(".") + 1);
+            stringBuffer.append(param).append(":").append(t.getMessage()).append(",");
+        });
+        String message = stringBuffer.substring(0, stringBuffer.length() - 1);
+        String RequestURL = req.getMethod() + " " + req.getRequestURI();
+        return new UnifyResponse(code, message, RequestURL);
+    }
+
+
 }
--- src/main/java/com/smart/mall/web/bannerController.java
+++ src/main/java/com/smart/mall/web/bannerController.java
@@ -1,20 +1,22 @@
 package com.smart.mall.web;
 
 import com.smart.mall.exception.http.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 
 @RestController
 @RequestMapping("/banner")
+@Validated
 public class bannerController {
 
     @GetMapping("/test")
     public void test(){
-        throw new NotFoundException(9999);
-    }
-    @GetMapping("/test2")
-    public void test2(){
         throw new RuntimeException();
     }
 }
