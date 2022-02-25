b2500a2b2aedd26b7382d787fb62d384e6b4e8d6
sujie <1453648423@qq.com>
2022-02-25 16:56:09 +0800

    引入SpuPureVO（用于解决导航属性惰性加载失效）

---
+++ src/main/java/com/smart/mall/vo/SpuPureVO.java
@@ -0,0 +1,14 @@
+package com.smart.mall.vo;
+
+public class SpuPureVO {
+    private Long id;
+    private String title;
+    private String subtitle;
+    private String price;
+    private Long sketchSpecId;
+    private String img;
+    private String discountPrice;
+    private String description;
+    private String tags;
+    private String forThemeImg;
+}
