f2dd67a7bde9234aa9d22d2fde3e184cafc4a546
sujie <1453648423@qq.com>
2022-04-14 15:28:32 +0800

    fix bug

--- src/main/java/com/smart/mall/vo/SpuPureVO.java
+++ src/main/java/com/smart/mall/vo/SpuPureVO.java
@@ -1,12 +1,16 @@
 package com.smart.mall.vo;
 
 import com.smart.mall.model.Spu;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.BeanUtils;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SpuPureVO {
     private Long id;
     private String title;
